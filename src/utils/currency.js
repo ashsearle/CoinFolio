f8aeb38af2a7bc4310ebf56a09cab4ab871da064
LGLabGreg <greg@lglab.co.uk>
2018-03-03 15:43:24 +0000

    fix exchangeToUserCurrency

--- src/utils/currency.js
+++ src/utils/currency.js
@@ -1,19 +1,15 @@
-export const exchangeToUserCurrency = (value, { fiatCurrency, exchangeRates }) => {
+export const exchangeToUserCurrency = (
+  value,
+  { fiatCurrency, exchangeRates }
+) => {
   const { value: currency } = fiatCurrency;
-  if (
-    !exchangeRates ||
-    !Object.keys(exchangeRates).length
-  ) {
+  if (!exchangeRates || !Object.keys(exchangeRates).length) {
     return value;
   }
-  return +value * exchangeRates[currency];
+  return +value * (exchangeRates[currency] || 1);
 };
 
-export const formatCurrency = (
-  { fiatCurrency },
-  value = 0,
-  options = {}
-) => {
+export const formatCurrency = ({ fiatCurrency }, value = 0, options = {}) => {
   const { value: currency, locales } = fiatCurrency;
   return new Intl.NumberFormat(locales, {
     style: 'currency',
